d9bd011f50885717b92192480833018b19114998
Claudia :) <clasan436@alu.centroafuera.es>
2022-05-24 10:05:54 +0200

    24-05-2022

--- src/test/java/PersonajeTest.java
+++ src/test/java/PersonajeTest.java
@@ -23,4 +23,51 @@ public class PersonajeTest {
         // tip: ej.personajes.obtenerPotters()
     }
 
+
+    @Test
+    public void test3(){
+        // TODO Crea una función que cuente a todos los personajes que forman parte del staff de Howarts y que están vivos
+        //
+        // TODO comprueba que tu función devuelve el número correcto de elementos
+
+    }
+
+    @Test
+    public void test4(){
+        // TODO Crea una función que cuente a todos los personajes que forman parte del staff de Howarts y que están muertos
+        //
+        // TODO comprueba que tu función devuelve el número correcto de elementos
+
+    }
+
+    @Test
+    public void test5(){
+        // TODO Crea una función que reciba una letra (char) y cuente todos personajes tienen esa letra en su nombre
+        //
+        // TODO comprueba que tu función devuelve el número correcto de elementos
+
+    }
+
+    @Test
+    public void test6(){
+        // TODO Crea una función que reciba un string y devuelva una lista con todos los personajes que tienen ese string
+        //  en su nombre y están vivos
+        //
+        // TODO comprueba que tu función devuelve el número correcto de elementos
+
+    }
+
+    @Test
+    public void test7(){
+        // TODO Crea una función que reciba un string y devuelva una lista con todos los personajes que su actor se llama
+        //  exactamente como el string recibido
+
+        // TODO comprueba que tu función devuelve el número correcto de elementos
+    }
+
+
+
+    // TODO Realiza todos los test que consideres para probar la función buscar por nombre. Esa función buscará a todos los
+    //  personajes cuyos nombres contengan el string introducido. Si buscas Voldermort tendras problemas.
+
 }
